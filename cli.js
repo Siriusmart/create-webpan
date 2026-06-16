956490ee332b49d938e7d06f92c2b04c7396c287
Siriusmart <sirius@siri.ws>
2026-06-16 16:44:28 +0100

    initial commit

---
+++ cli.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import { mkdirSync, cpSync, renameSync } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const cwd = process.cwd();
+const projectName = process.argv[2] || 'my-webpan-project';
+const target = path.join(cwd, projectName);
+
+mkdirSync(target, { recursive: true });
+
+// locate the template directory inside your package
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const templateDir = path.join(__dirname, 'template');
+
+cpSync(templateDir, target, { recursive: true });
+renameSync(path.join(target, "gitignore"), path.join(target, ".gitignore"));
+
+console.log(`Created ${projectName}`);
